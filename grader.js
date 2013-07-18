13218b5bb2004d5035b4e31d408d7a0aa9ad771c
Trevor Austin <traustin@gmail.com>
2013-07-18 15:24:08 +0000

    Added Phone Press title and subhead

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var TEMPFILE = "temp.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,11 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertPageExists = function(url_path) {
+    console.log("Can't open the URL %s.  Exiting.", url_path);
+    process.exit(1);
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,10 +72,22 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_path>', 'URL to load page from')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    if(program.url) {
+        rest.get(program.url).on('complete', function(data) {
+            fs.writeFileSync(TEMPFILE, data);
+            var checkJson = checkHtmlFile(TEMPFILE, program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        });
+    }
+    else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
